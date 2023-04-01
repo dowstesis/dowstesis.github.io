a28795e7e61a63d384801f8ea5a4190ea34f1c9b
15ivan15 <ivanmolina15xd1@gmail.com>
2023-03-31 23:40:00 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@
 var datos; // Variable global para almacenar los datos de la tabla
 var datos2;
 var filtrado = [];
+var xx = 0;
 document.getElementById("contenido").innerHTML = recomendaciones();
 
-var xx = 0;
 var rep = document.getElementById("reporte");
 rep.disabled = true;
 var cand = document.getElementById("candidatos");
