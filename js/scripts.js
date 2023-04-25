29f88c660fad163394a2ed98db92cc6fb5385bae
15ivan15 <ivanmolina15xd1@gmail.com>
2023-04-25 13:24:16 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -129,7 +129,7 @@ function mostrarCandidatos() {
         var gas = datos[i][datos[0].indexOf("Prod Gas Anular (Kpcd)")];
         gas_intake = gas / prodtotal;
         // Verificar que producción total sea mayor a 1000 bbl/day
-        // el corte de agua sea mayor al 60%, la gravedad API sea mayor a 16
+        // el corte de agua sea mayor al 80%, la gravedad API sea mayor a 20
         // y la entrada de gas sea menor al 2%
         if (prodtotal > 1000 && bsw > 0.6 && api > 16 && gas_intake < 0.02) {
             filtrado[t] = new Array(datos[0].length);
